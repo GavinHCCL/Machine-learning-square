d65ed83bb3c3c92ef73c69004dea6bb3f9ac5c9b
GavinHCCL <89994967+GavinHCCL@users.noreply.github.com>
2022-12-21 16:16:10 -0800

    Update 0.1
    This code generates a random maze using a depth-first search algorithm, initializes a Q-learning model, and then uses the model to navigate the maze. The dot is represented by the current state, and the Q-learning model is used to find the exit.

--- square.py
+++ square.py
@@ -1,112 +1,88 @@
-import pygame
-import numpy as np
-import time
+import random
 
-# Initialize pygame
-pygame.init()
+# Generate a random maze using depth-first search
+def generate_maze(maze_size):
+  maze = [[0 for _ in range(maze_size)] for _ in range(maze_size)]
+  stack = []
+  visited = set()
+  current_cell = (0, 0)
+  maze[current_cell[0]][current_cell[1]] = 1
+  visited.add(current_cell)
+  stack.append(current_cell)
 
-# Set the window size
-window_size = (500, 500)
+  while stack:
+    neighbors = []
+    x, y = current_cell
+    for dx, dy in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+      new_x, new_y = x + dx, y + dy
+      if (0 <= new_x < maze_size) and (0 <= new_y < maze_size) and ((new_x, new_y) not in visited):
+        neighbors.append((new_x, new_y))
+    if neighbors:
+      new_cell = random.choice(neighbors)
+      maze[new_cell[0]][new_cell[1]] = 1
+      visited.add(new_cell)
+      stack.append(new_cell)
+      current_cell = new_cell
+    else:
+      current_cell = stack.pop()
+  return maze
 
-# Create the window
-screen = pygame.display.set_mode(window_size)
+# Initialize Q-learning model
+def initialize_q_learning(maze_size):
+  q_values = {}
+  for i in range(maze_size):
+    for j in range(maze_size):
+      q_values[(i, j)] = {'up': 0, 'down': 0, 'left': 0, 'right': 0}
+  return q_values
 
-# Set the title of the window
-pygame.display.set_caption("Dot Window")
+# Select the next action using the Q-learning model
+def select_action(q_values, current_state, epsilon):
+  if random.random() < epsilon:
+    return random.choice(['up', 'down', 'left', 'right'])
+  else:
+    return max(q_values[current_state], key=q_values[current_state].get)
 
-# Set the starting position of the black square
-square_pos = (320, 300)
+# Update the Q-learning model based on the current state and action
+def update_q_values(q_values, current_state, action, reward, next_state, alpha, gamma):
+  q_values[current_state][action] = (1 - alpha) * q_values[current_state][action] + alpha * (reward + gamma * max(q_values[next_state].values()))
 
-# Set the reward for reaching the green dot
-reward = 1
+# Main function to navigate the maze
+def navigate_maze(maze, q_values, alpha, gamma, epsilon, max_steps):
+  current_state = (0, 0) # start at the top-left corner
+  steps = 0
+  while current_state != (len(maze) - 1, len(maze) - 1): # stop when we reach the bottom-right corner
+    action = select_action(q_values, current_state, epsilon)
+    x, y = current_state
+    if action == 'up':
+      next_state = (x - 1, y)
+      reward = -1 # penalize movement
+    elif action == 'down':
+      next_state = (x + 1, y)
+      reward = -1
+    elif action == 'left':
+      next_state = (x, y - 1)
+      reward = -1
+    elif action == 'right':
+      next_state = (x, y + 1)
+      reward = -1
+    if next_state[0] < 0 or next_state[0] >= len(maze) or next_state[1] < 0 or next_state[1] >= len(maze) or maze[next_state[0]][next_state[1]] == 0:
+      reward = -10 # penalize attempts to move out of the maze
+      next_state = current_state
+    update_q_values(q_values, current_state, action, reward, next_state, alpha, gamma)
+    current_state = next_state
+    steps += 1
+    if steps >= max_steps:
+      break
+  return steps
 
-# Set the punishment for moving away from the green dot
-punishment = -0.1
-
-# Set the learning rate
+# Test the maze navigation
+maze_size = 10
+maze = generate_maze(maze_size)
+q_values = initialize_q_learning(maze_size)
 alpha = 0.1
+gamma = 0.9
+epsilon = 0.1
+max_steps = 1000
 
-# Initialize the Q-table
-q_table = np.zeros((500, 500))
-
-# Set the timer to 10 seconds
-timer = 10
-
-# Run the game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # Decrement the timer by 1 every iteration
-    timer -= 1
-
-    # Check if the timer has reached 0
-    if timer == 0:
-        # Reset the timer and the position of the black square
-        timer = 10
-        square_pos = (320, 300)
-
-        # Apply the punishment
-        q_table[square_pos[0], square_pos[1]] += punishment
-
-    # Clear the screen
-    screen.fill((255, 255, 255))
-
-    # Draw the green dot
-    pygame.draw.circle(screen, (0, 255, 0), (180, 200), 5)
-
-    # Draw the black square
-    pygame.draw.rect(screen, (0, 0, 0), (square_pos[0], square_pos[1], 10, 10))
-
-    # Check if the timer has reached 0
-    if timer == 0:
-        # Check if the black square has reached the green circle
-        if square_pos != (180, 200):
-            # Reset the timer and the position of the black square
-            timer = 10
-            square_pos = (320, 300)
-
-            # Apply the punishment
-            q_table[square_pos[0], square_pos[1]] += punishment
-
-    # Calculate the distance between the black square and the green dot
-    distance = np.sqrt((square_pos[0] - 180)**2 + (square_pos[1]))
-
-    # Check if the black square has moved out of the window
-    if square_pos[0] < 0 or square_pos[0] >= 500 or square_pos[1] < 0 or square_pos[1] >= 500:
-        continue
-
-    # Update the Q-value for the current state
-    q_table[square_pos[0], square_pos[1]] = q_table[square_pos[0], square_pos[1]] + alpha * (reward + q_table[180, 200] - q_table[square_pos[0], square_pos[1]])
-
-    # Choose the action with the highest Q-value
-    action = np.argmax(q_table[square_pos[0], square_pos[1]])
-
-
-    # Update the Q-values of the neighboring states
-    for i, action in enumerate(range(4)):
-        if action == 0:
-            next_state = (square_pos[0] - 1, square_pos[1])
-        elif action == 1:
-            next_state = (square_pos[0] + 1, square_pos[1])
-        elif action == 2:
-            next_state = (square_pos[0], square_pos[1] - 1)
-        elif action == 3:
-            next_state = (square_pos[0], square_pos[1] + 1)
-
-        # Check if the next state is within the bounds of the window
-        if next_state[0] >= 0 and next_state[0] < 500 and next_state[1] >= 0 and next_state[1] < 500:
-            q_table[next_state[0], next_state[1]] = q_table[next_state[0], next_state[1]] + alpha * (reward + q_table[180, 200] - q_table[next_state[0], next_state[1]])
-
-            # Punish the black square for moving away from the green dot
-    if distance < np.sqrt((square_pos[0] - 180)**2 + (square_pos[1] - 200)**2):
-        q_table[square_pos[0], square_pos[1]] += punishment
-
-    # Update the display
-    pygame.display.update()
-
-    # Sleep for 10 milliseconds
-    time.sleep(0.01)
-
+steps = navigate_maze(maze, q_values, alpha, gamma, epsilon, max_steps)
+print(f'Number of steps taken to reach the finish: {steps}')
