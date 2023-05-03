2bf5f0ff83b5fdb44db0b3f5772fccd25923a00f
GavinHCCL <89994967+GavinHCCL@users.noreply.github.com>
2023-05-03 10:59:57 -0700

    Update square.py

--- square.py
+++ square.py
@@ -1,6 +1,6 @@
 import random
 
-# Generate a random maze using depth-first search
+# Generate a random maze
 def generate_maze(maze_size):
   maze = [[0 for _ in range(maze_size)] for _ in range(maze_size)]
   stack = []
@@ -27,7 +27,7 @@ def generate_maze(maze_size):
       current_cell = stack.pop()
   return maze
 
-# Initialize Q-learning model
+# Q LEARNING YAYYYYY
 def initialize_q_learning(maze_size):
   q_values = {}
   for i in range(maze_size):
@@ -35,27 +35,25 @@ def initialize_q_learning(maze_size):
       q_values[(i, j)] = {'up': 0, 'down': 0, 'left': 0, 'right': 0}
   return q_values
 
-# Select the next action using the Q-learning model
 def select_action(q_values, current_state, epsilon):
   if random.random() < epsilon:
     return random.choice(['up', 'down', 'left', 'right'])
   else:
     return max(q_values[current_state], key=q_values[current_state].get)
 
-# Update the Q-learning model based on the current state and action
 def update_q_values(q_values, current_state, action, reward, next_state, alpha, gamma):
   q_values[current_state][action] = (1 - alpha) * q_values[current_state][action] + alpha * (reward + gamma * max(q_values[next_state].values()))
 
-# Main function to navigate the maze
+
 def navigate_maze(maze, q_values, alpha, gamma, epsilon, max_steps):
-  current_state = (0, 0) # start at the top-left corner
+  current_state = (0, 0) 
   steps = 0
-  while current_state != (len(maze) - 1, len(maze) - 1): # stop when we reach the bottom-right corner
+  while current_state != (len(maze) - 1, len(maze) - 1):
     action = select_action(q_values, current_state, epsilon)
     x, y = current_state
     if action == 'up':
       next_state = (x - 1, y)
-      reward = -1 # penalize movement
+      reward = -1
     elif action == 'down':
       next_state = (x + 1, y)
       reward = -1
@@ -66,7 +64,7 @@ def navigate_maze(maze, q_values, alpha, gamma, epsilon, max_steps):
       next_state = (x, y + 1)
       reward = -1
     if next_state[0] < 0 or next_state[0] >= len(maze) or next_state[1] < 0 or next_state[1] >= len(maze) or maze[next_state[0]][next_state[1]] == 0:
-      reward = -10 # penalize attempts to move out of the maze
+      reward = -10 
       next_state = current_state
     update_q_values(q_values, current_state, action, reward, next_state, alpha, gamma)
     current_state = next_state
